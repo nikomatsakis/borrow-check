ef3bb5c25df7dda2f05c2239494b66f100f05c34
Chris Vittal <christopher.vittal@gmail.com>
2018-05-03 13:25:56 -0400

    Add a few more tests

--- relation/src/test.rs
+++ relation/src/test.rs
@@ -80,3 +80,38 @@ fn add_remove_2() {
 
     test(&r, &["N(0) --E(0)--> N(1)", "free edge E(1)"]);
 }
+
+#[test]
+fn add_cycle() {
+    let n0: NodeIndex = NodeIndex::from(0);
+    let n1: NodeIndex = NodeIndex::from(1);
+    let n2: NodeIndex = NodeIndex::from(2);
+    let mut r = StdVecRelation::new(3);
+
+    r.add_edge(n0, n1);
+    r.add_edge(n1, n2);
+    r.add_edge(n2, n0);
+
+    test(&r, &["N(0) --E(0)--> N(1)",
+               "N(1) --E(1)--> N(2)",
+               "N(2) --E(2)--> N(0)",
+              ]);
+}
+
+#[test]
+fn add_remove_cycle() {
+    let n0: NodeIndex = NodeIndex::from(0);
+    let n1: NodeIndex = NodeIndex::from(1);
+    let n2: NodeIndex = NodeIndex::from(2);
+    let mut r = StdVecRelation::new(3);
+
+    r.add_edge(n0, n1);
+    r.add_edge(n1, n2);
+    r.add_edge(n2, n0);
+    r.remove_edges(n1);
+
+    test(&r, &["N(0) --E(0)--> N(2)",
+               "N(2) --E(2)--> N(0)",
+               "free edge E(1)",
+              ]);
+}
